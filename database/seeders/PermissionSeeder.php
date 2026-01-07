6a3c21fd584be1525b3360b21e6653027ab8dd61
a3mqq2 <aishaaltery03@gmail.com>
2026-01-08 00:38:42 +0200

    updates

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,12 +4,23 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
 
 class PermissionSeeder extends Seeder
 {
     public function run()
     {
+        // مسح الكاش
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // حذف جميع الصلاحيات القديمة
+        DB::table('model_has_permissions')->delete();
+        DB::table('role_has_permissions')->delete();
+        Permission::query()->delete();
+
+        // الصلاحيات الجديدة
         $permissions = [
             ['name' => 'invoices', 'name_ar' => 'المناسبات'],
             ['name' => 'treasury', 'name_ar' => 'الخزينة'],
@@ -21,10 +32,11 @@ class PermissionSeeder extends Seeder
         ];
 
         foreach ($permissions as $item) {
-            Permission::firstOrCreate(
-                ['name' => $item['name']],
-                ['name_ar' => $item['name_ar'], 'guard_name' => 'web']
-            );
+            Permission::create([
+                'name' => $item['name'],
+                'name_ar' => $item['name_ar'],
+                'guard_name' => 'web'
+            ]);
         }
 
         // منح المستخدم الأول جميع الصلاحيات
